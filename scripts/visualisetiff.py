d76dcdc84d403b769fd0bae547762d0a7de77496
s2762697 <s2762697@ed.ac.uk>
2025-04-01 11:19:43 +0100

    colour and DEM

--- scripts/visualisetiff.py
+++ scripts/visualisetiff.py
@@ -1,27 +1,20 @@
 import rasterio     #higher level package raster functions
 import matplotlib.pyplot as plt
+import numpy as np
 
-dataset = rasterio.open('lvisDEMloop.x277.45731593434334.y.-68.80784508465653.tif')
 
-
-print(dataset.width)    #print width of raster
-print(dataset.height)   #print height of raster
+dataset = rasterio.open('../Data/lvisDEM_PIG6.tif')
+# print raster dimensions
+print(f'The raster is {dataset.width} x {dataset.height}  pixels')
 print(dataset.crs)      #print projection
 
-lvis_image = dataset.read(1)
-#lvis_image == [-999.0 = nan]
-plt.imshow(lvis_image)
-plt.colorbar()      #legend
-#plt.xlabel('Longitude')
-#plt.ylabel('Latitude')
+lvis_image = dataset.read(1)  #read band
+
+# set no values to not a number
+lvis_image = np.where(lvis_image ==-999.0, np.nan, lvis_image)
+plt.imshow(lvis_image, cmap='cividis')
+plt.colorbar(label='Elevation (m)')      # legend
+plt.xlabel('metres') # check this
+plt.ylabel('metres')  # check this
 plt.show()
-
-#is this visualising on elevation 
-
-
-#new_dataset = rasterio.open(outName,'w', driver = 'GTiff', height = dataset8.height,
-#                            width = dataset8.width, count = 1, dtype = NDVI_image.dtype,
-#                            crs = dataset8.crs, ransform = dataset8.transform)
-#new_dataset.write(NDVI_image, 1)
-#new_dataset.nodata = -999.0
-#new_dataset.close()
+#print(np.unique(lvis_image))
