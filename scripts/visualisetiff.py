20c75834b004cdae5b68efe109c9e1a033b04551
s2762697 <s2762697@ed.ac.uk>
2025-04-01 15:26:55 +0100

    adds functions

--- scripts/visualisetiff.py
+++ scripts/visualisetiff.py
@@ -6,56 +6,60 @@ import glob
 import os
 
 #eventually change this so that visualise tiff is also in plot class???
+def merge_tiles(input_dir, output_path):
 
-# input and output directories
-tiles_dir2009 = '../Data'
-output_folder = '../Outputs'
+    # call all tif files in input folder
+    tile_files = glob.glob(os.path.join(input_dir,'*tif'))
+
+    # open tiles
+    src_files_to_mosaic = []    # create empty array
+    for fp in tile_files:       # loop over file paths
+        src = rasterio.open(fp)
+        src_files_to_mosaic.append(src)
+
+    # merge with rasterio + georeference
+    mosaic, out_trans = merge(src_files_to_mosaic)
+
+    # set mo data to NaN before export
+    mosaic = np.where(mosaic == -999.0, np.nan, mosaic)
+
+    # save merged file
+    with rasterio.open(output_tiff, "w", driver='GTiff', #create output_tiff, specify format
+                       height=mosaic.shape[1],  # tiff is same height as input
+                       width=mosaic.shape[2],   # tiff is same width as input
+                       count=1, dtype=rasterio.float32,     # can handle NaN
+                       crs=src_files_to_mosaic[0].crs,  # uses same crs as input file
+                       transform=out_trans) as dest:
+                        dest.write(mosaic[0], 1)    # writes to first band 
+
+    for src in src_files_to_mosaic:     # closes to free space 
+            src.close()
+
+    print(f'Merged DEM is saved as {output_tiff}')
+
+def visualise_tiff(tiff_path):
+    # open the tiff to visualise with rasterio
+    dataset = rasterio.open(tiff_path)
+    # print raster dimensions
+    print(f'The raster is {dataset.width} x {dataset.height}  pixels')
+    print(dataset.crs)      #print projection
+
+    merged_image = dataset.read(1)  #read band
+
+    # make plot
+    plt.imshow(merged_image, cmap='cividis')
+    plt.colorbar(label='Elevation (m)')      # legend
+    plt.xlabel('metres')
+    plt.ylabel('metres')
+    plt.show()
+    #print(np.unique(lvis_image))
+
+# specify places
+tiles_dir2009 = '../Data'   # specify input dir
+output_folder = '../Outputs'    # specify output dir
+os.makedirs(output_folder, exist_ok=True) # check folder exists and create if not
 output_tiff = os.path.join(output_folder, '2009_DEM')
 
-# check folder exists and create if not
-os.makedirs(output_folder, exist_ok=True)
-
-
-# call all tif files in input folder
-tile_files = glob.glob(os.path.join(tiles_dir2009,'*tif'))
-
-# open tiles
-src_files_to_mosaic = []    # create empty array
-for fp in tile_files:       # loop over file paths
-    src = rasterio.open(fp)
-    src_files_to_mosaic.append(src)
-
-# merge with rasterio + georeference
-mosaic, out_trans = merge(src_files_to_mosaic)
-
-# set mo data to NaN before export
-mosaic = np.where(mosaic == -999.0, np.nan, mosaic)
-
-# save merged file
-with rasterio.open(output_tiff, "w", driver='GTiff', #create output_tiff, specify format
-                   height=mosaic.shape[1],  # tiff is same height as input
-                   width=mosaic.shape[2],   # tiff is same width as input
-                   count=1, dtype=rasterio.float32,
-                   crs=src_files_to_mosaic[0].crs,
-                   transform=out_trans) as dest:
-                    dest.write(mosaic[0], 1)
-
-for src in src_files_to_mosaic:
-        src.close()
-
-#print(f'Merged DEM is saved as {output_tiff}')
-
-dataset = rasterio.open('../Outputs/2009_DEM')
-# print raster dimensions
-print(f'The raster is {dataset.width} x {dataset.height}  pixels')
-print(dataset.crs)      #print projection
-
-merged_image = dataset.read(1)  #read band
-
-# make plot
-plt.imshow(merged_image, cmap='cividis')
-plt.colorbar(label='Elevation (m)')      # legend
-plt.xlabel('metres') # check this
-plt.ylabel('metres')  # check this
-plt.show()
-#print(np.unique(lvis_image))
+# call functions
+merge_tiles(tiles_dir2009, output_tiff)
+visualise_tiff(output_tiff)
