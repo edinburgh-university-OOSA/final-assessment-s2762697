423356e8f2f5cc06cba4d3db5ac72a39e06dcdab
s2762697 <s2762697@ed.ac.uk>
2025-04-03 17:21:50 +0100

    parser

--- scripts/lvisExample.py
+++ scripts/lvisExample.py
@@ -1,17 +1,17 @@
-
 '''An example of how to use the LVIS python scripts'''
 
 # import libraries
-#from lvisClass import lvisData
 from pyproj import Transformer
 from matplotlib import pyplot as plt
 from glob import glob
 import tracemalloc
 import argparse
+import os
 
 #import other classes and functions
 from processLVIS import lvisGround
 from tiffExample import writeTiff
+from visualisetiff import HandleTiff
 
 ##########################################
 
@@ -20,12 +20,24 @@ def chooseArg():
   # create argparse object
   parser = argparse.ArgumentParser(description='Choose a single waveform from LVIS data')
   # waveform index
-  parser.add_argument('-i', '--index', type=int, default=155892, help = 'Index of waveform')
+  parser.add_argument('-i', type=int, default=155892, help = 'Index of waveform')
   # waveform file name 
-  parser.add_argument('-o', '--output', type=str, default='Waveform.png',
+  parser.add_argument('-o', type=str, default='Waveform.png',
                       help='Output file name of waveform')
+  # merged DEM tiff name
+  parser.add_argument('--tiff_output', type=str, default='2009_DEM.tif', help='Name of the output TIFF file')
+  # resolution with threshold
+  parser.add_argument('--res', type=int, default=30, help='Resolution for DEM creation (between 20 and 100)')
+
+
   # parse the command line into an object
   args = parser.parse_args()
+
+  # check threshold for resolution 
+  if not(20<= args.res <= 100):
+    print("Warning, resolution must be between 20 and 100. Using default (30)")
+    args.res = 30 
+
   return args
 
 class plotLVIS(lvisGround):
@@ -44,14 +56,12 @@ class plotLVIS(lvisGround):
     except IndexError as e: #raise error if no data
       print(e)
 
-
   def makeDEM(self, resolution, tiffName):
     '''convert data to a goetiff (from tiffExample.py)'''
     # call function from tiffExample.py
     writeTiff(data=self.zG, x=self.x, y=self.y, res=resolution, filename=tiffName, epsg=3031)
     return 
 
-# change back to zgravity
 ##########################################
 
 if __name__=="__main__":
@@ -70,7 +80,7 @@ if __name__=="__main__":
   #print(filelist_2015)
 
   # file over Pine Island Glacier used in Task 1 and Task 2
-  filename='/geos/netdata/oosa/assignment/lvis/2009/ILVIS1B_AQ2009_1020_R1408_058456.h5'  #pine yay 
+  filename='/geos/netdata/oosa/assignment/lvis/2009/ILVIS1B_AQ2009_1020_R1408_058456.h5'  #single file test
 
 ##############################################
   # find bounds
@@ -105,19 +115,29 @@ if __name__=="__main__":
       lvis.reprojectLVIS(3031)  # call reproject function -move to process
       lvis.estimateGround()     # call find ground function
       tiffName = f"../Data/lvisDEM_PIG{tile_number}.tif"   #filename with tile identifier
-      lvis.makeDEM(100, tiffName)       #resolution 
+      lvis.makeDEM(command.res, tiffName)       #resolution 
 
       # tile counter at end of loop
       tile_number +=1   #after it is called
 
   # Outside loop
   # call function to plot single waveform
-  lvis.plotSingleWave(index=command.index)
-
-  print(f'Each geotiff is split into {subset_size} x {subset_size} tiles') 
+ #lvis.plotSingleWave(index=command.index)
+print(f'Each geotiff is split into {subset_size} x {subset_size} tiles') 
   
-  # Memory tracking
-  current, peak = tracemalloc.get_traced_memory() #both needed as returns a tuple
-  peak_GB = peak/(1024**3)
-  print(f'Peak memory used was: {peak_GB:.2f} GB')
-  #print(current,peak)
+# specify places
+tiles_dir2009 = '../Data'   # specify input dir
+output_folder = '../Outputs'    # specify output dir
+os.makedirs(output_folder, exist_ok=True) # check folder exists and create if not
+output_tiff = os.path.join(output_folder, command.tiff_output)
+
+# call functions
+get_tiff = HandleTiff()
+get_tiff.merge_tiles(tiles_dir2009, output_tiff)
+get_tiff.visualise_tiff(output_tiff)
+
+# Memory tracking
+current, peak = tracemalloc.get_traced_memory() #both needed as returns a tuple
+peak_GB = peak/(1024**3)
+print(f'Peak memory used was: {peak_GB:.2f} GB')
+#print(current,peak)
